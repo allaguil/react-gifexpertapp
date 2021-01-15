1c4797903258c71808d7d4d85297fc727edc7355
Allan Aguilar <alan-joc@hotmail.com>
2021-01-15 10:48:14 -0600

    index.html fixed

---
+++ docs/precache-manifest.b30dc5b234f844e9674620abae08e57b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871728058c129d2e2e67e32e92286dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ccc826434a536c13430",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ccc826434a536c13430",
+    "url": "/static/js/main.ffb94405.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
